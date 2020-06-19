df96afa53eeea25435a83c8e2be67fd2fcab9752
Stralle <strahinja.rodic@hotmail.com>
2020-06-19 19:48:48 +0200

    added localization files and managers

---
+++ Class 8 - UI Project/Assets/Scripts/LocKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This script should be attached to each gameobject we want to be localized!
+public class LocKey : MonoBehaviour
+{
+    [SerializeField] private string m_locKey;
+
+    private Text m_UIText;
+
+    void Start()
+    {
+        m_UIText = GetComponent<Text>();
+
+        if (m_UIText && m_locKey != "")
+        {
+            m_UIText.text = LocalizationManager.instance.GetLocalizedString(m_locKey);
+        }
+    }
+}
---
+++ Class 8 - UI Project/Assets/Scripts/LocalizationManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public enum ELanguage
+{
+    English,
+    Serbian
+}
+public class LocalizationManager : MonoBehaviour
+{
+    public static LocalizationManager instance = null;
+
+    [SerializeField]
+    private ELanguage m_currentLanguage = ELanguage.English;
+
+    private Dictionary<ELanguage, TextAsset> m_localizationFiles = new Dictionary<ELanguage, TextAsset>();
+    private Dictionary<string, string> m_localizationData = new Dictionary<string, string>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Localization files/data should be loaded in Awake, to make sure everything is loaded on time!
+        DontDestroyOnLoad(gameObject);
+        SetupLocalizationFiles();
+        SetupLocalizationData();
+    }
+
+    void SetupLocalizationFiles()
+    {
+        foreach (ELanguage language in ELanguage.GetValues(typeof(ELanguage)))
+        {
+            string textAssetPath = "Localization/" + language; 
+            TextAsset textAsset = (TextAsset) Resources.Load(textAssetPath); // It looks for the file in the folder Resources by default, so now the path is Resources/Localization
+            if (textAsset)
+            {
+                m_localizationFiles[language] = textAsset;
+                Debug.Log("Text Asset: " + textAsset.name);
+            }
+            else
+            {
+                Debug.LogError("Text Asset not found " + textAssetPath);
+            }
+        }
+    }
+
+    void SetupLocalizationData()
+    {
+        TextAsset textAsset;
+        if (m_localizationFiles.ContainsKey(m_currentLanguage))
+        {
+            Debug.Log("Selected language found: " + m_currentLanguage);
+            textAsset = m_localizationFiles[m_currentLanguage];
+        }
+        else
+        {
+            Debug.LogError("Couldn't find localization file for: " + m_currentLanguage);
+            textAsset = m_localizationFiles[ELanguage.English];
+        }
+
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(textAsset.text);
+
+        XmlNodeList entryList = xmlDocument.GetElementsByTagName("Entry");
+
+        foreach (XmlNode entry in entryList)
+        {
+            if (!m_localizationData.ContainsKey(entry.FirstChild.InnerText))
+            {
+                Debug.Log("Added key " + entry.FirstChild.InnerText + " with value " + entry.LastChild.InnerText);
+                m_localizationData.Add(entry.FirstChild.InnerText, entry.LastChild.InnerText);
+            }
+            else
+            {
+                Debug.LogError("Duplicate key found for: " + entry.FirstChild.InnerText);
+            }
+        }
+    }
+
+    public string GetLocalizedString(string key)
+    {
+        string localizedString = "";
+        if (!m_localizationData.TryGetValue(key, out localizedString))
+        {
+            localizedString = "LOC KEY: " + key;
+        }
+
+        return localizedString;
+    }
+}
