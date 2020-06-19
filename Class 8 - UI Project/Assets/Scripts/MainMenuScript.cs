38eae82334337cc7ef4d4462cf5eedab580bc9e3
Stralle <strahinja.rodic@hotmail.com>
2020-06-19 13:08:45 +0200

    Created main menu UI with added animation of knight

---
+++ Class 8 - UI Project/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartClicked()
+    {
+        Debug.Log("Start clicked");
+    }
+
+    public void OnOptionsClicked()
+    {
+        Debug.Log("Options clicked");
+    }
+
+    public void OnQuitClicked()
+    {
+        Debug.Log("Quit clicked");
+    }
+}
