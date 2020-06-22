40c9e1270a361f7ccc6253d42a1a95cceb59e3a6
Stralle <strahinja.rodic@hotmail.com>
2020-06-22 18:46:12 +0200

    Added line to track where the player is aiming, added IK animations to move player's shoulder and neck to where he's aiming

---
+++ Class 9 - Advanced Animations/Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+    [SerializeField]
+    float m_cameraTargetYOffset = 2.0f;
+    [SerializeField]
+    float m_distanceZOffset = -6.0f;
+    [SerializeField]
+    float m_rotationX = 6.1f;
+    [SerializeField]
+    float m_rotationY = 0.0f;
+
+    Vector3 m_cameraTarget;
+    GameObject m_player;
+    const float MIN_X = -25.0f;
+    const float MAX_X = 25.0f;
+    const float MIN_Z = -8f;
+    const float MAX_Z = -3.2f;
+
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        m_cameraTarget = m_player.transform.position;
+        m_cameraTarget.y += m_cameraTargetYOffset;
+
+        if (Input.GetButton("Fire2"))
+        {
+            m_rotationY += Input.GetAxis("Mouse X"); // Rotating around the Y axis.
+            m_rotationX -= Input.GetAxis("Mouse Y"); // Rotating around the X axis
+        }
+
+        m_distanceZOffset += Input.GetAxis("Mouse ScrollWheel");
+        m_distanceZOffset = Mathf.Clamp(m_distanceZOffset, MIN_Z, MAX_Z);
+        m_rotationX = Mathf.Clamp(m_rotationX, MIN_X, MAX_X);
+    }
+
+    private void LateUpdate()
+    {
+        Quaternion cameraRotation = Quaternion.Euler(m_rotationX, m_rotationY, 0);
+        Vector3 cameraOffset = new Vector3(0, 0, m_distanceZOffset);
+        transform.position = m_cameraTarget + cameraRotation * cameraOffset; // Order is important. Multiply rotation * offset and not vice versa!
+        transform.LookAt(m_cameraTarget);
+    }
+
+    public Vector3 GetForwardVector()
+    {
+        Quaternion rotation = Quaternion.Euler(0, m_rotationY, 0);
+        return rotation * Vector3.forward;
+    }
+
+    public float GetRotationX()
+    {
+        return m_rotationX;
+    }
+}
---
+++ Class 9 - Advanced Animations/Assets/Scripts/WeaponLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLogic : MonoBehaviour
+{
+    [SerializeField] private Transform m_bulletSpawnPosition;
+    [SerializeField] private float m_lineRendererLength = 1.0f;
+
+    private LineRenderer m_lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLineRenderer();
+    }
+
+    void UpdateLineRenderer()
+    {
+        if (m_lineRenderer)
+        {
+            m_lineRenderer.SetPosition(0, m_bulletSpawnPosition.position);
+            m_lineRenderer.SetPosition(1, m_bulletSpawnPosition.position + m_bulletSpawnPosition.transform.forward * m_lineRendererLength);
+        }
+    }
+}
