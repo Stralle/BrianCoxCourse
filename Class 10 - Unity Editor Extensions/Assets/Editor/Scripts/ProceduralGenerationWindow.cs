5a4605d0c063d21a16be71f53901c4c3a98306e1
Stralle <strahinja.rodic@hotmail.com>
2020-06-29 17:10:51 +0200

    Added tree models and generating x amount of trees on the scene

--- Class 10 - Unity Editor Extensions/Assets/Editor/Scripts/ProceduralGenerationWindow.cs
+++ Class 10 - Unity Editor Extensions/Assets/Editor/Scripts/ProceduralGenerationWindow.cs
@@ -18,6 +18,8 @@ public class ProceduralGenerationWindow : EditorWindow
     private string m_minRotZ = "0";
     private string m_maxRotZ = "0";
 
+    private string m_amountOfObjects = "1";
+
     [MenuItem("Tools/Spawn/Procedural Generation")]
     static void SpawnObject()
     {
@@ -27,24 +29,47 @@ public class ProceduralGenerationWindow : EditorWindow
 
     private void OnGUI()
     {
-        GUILayout.Label("Select an object you would like to spawn.");
+        GUILayout.Label("Select the objects you would like to spawn.");
 
-        if (!Selection.activeGameObject)
+        if (Selection.gameObjects.Length <= 0)
         {
             return;
         }
 
-        GUILayout.Label("You have selected: " + Selection.activeGameObject.name); 
+        string selectedObjectNames = GetSelectedObjectNames(Selection.gameObjects);
+        GUILayout.Label("You have selected: " + selectedObjectNames); 
 
         SetupPosition();
         SetupRotation();
 
+        SetupAmount();
+
         if (GUILayout.Button("Spawn Object"))
         {
-            SpawnObjects();
+            SpawnObjects(Selection.gameObjects, int.Parse(m_amountOfObjects));
         }
 
         Repaint(); // Refresh the window GUI. 
+    }
+
+    string GetSelectedObjectNames(GameObject[] selectedObjects)
+    {
+        string result = "";
+        for (int i = 0; i < selectedObjects.Length; ++i)
+        {
+            result += selectedObjects[i].name;
+
+            if (i < selectedObjects.Length - 1)
+            {
+                result += ",";
+            }
+            else
+            {
+                result += ".";
+            }
+        }
+
+        return result;
     }
 
     void SetupPosition()
@@ -75,18 +100,38 @@ public class ProceduralGenerationWindow : EditorWindow
         m_maxRotZ = EditorGUILayout.TextField("Max Z: ", m_maxRotZ);
     }
 
-    void SpawnObjects()
+    void SetupAmount()
     {
-        float posX = Random.Range(float.Parse(m_minX), float.Parse(m_maxX));
-        float posY = Random.Range(float.Parse(m_minY), float.Parse(m_maxY));
-        float posZ = Random.Range(float.Parse(m_minZ), float.Parse(m_maxZ));
 
-        float rotX = Random.Range(float.Parse(m_minRotX), float.Parse(m_maxRotX));
-        float rotY = Random.Range(float.Parse(m_minRotY), float.Parse(m_maxRotY));
-        float rotZ = Random.Range(float.Parse(m_minRotZ), float.Parse(m_maxRotZ));
+        GUILayout.Label("How many objects do you want to spawn?");
 
-        Quaternion rotation = Quaternion.Euler(new Vector3(rotX, rotY, rotZ));
+        m_amountOfObjects = EditorGUILayout.TextField("Number of objects: ", m_amountOfObjects);
+    }
 
-        Instantiate(Selection.activeGameObject, new Vector3(posX, posY, posZ), rotation, null);
+    void SpawnObjects(GameObject[] gameObjects, int amount)
+    {
+        GameObject parentObj = new GameObject("SpawnParentObject");
+        for (int index = 0; index < amount; ++index)
+        {
+            float posX = Random.Range(float.Parse(m_minX), float.Parse(m_maxX));
+            float posY = Random.Range(float.Parse(m_minY), float.Parse(m_maxY));
+            float posZ = Random.Range(float.Parse(m_minZ), float.Parse(m_maxZ));
+
+            float rotX = Random.Range(float.Parse(m_minRotX), float.Parse(m_maxRotX));
+            float rotY = Random.Range(float.Parse(m_minRotY), float.Parse(m_maxRotY));
+            float rotZ = Random.Range(float.Parse(m_minRotZ), float.Parse(m_maxRotZ));
+
+            Quaternion rotation = Quaternion.Euler(new Vector3(rotX, rotY, rotZ));
+
+            int randomObjectIndex = Random.Range(0, gameObjects.Length);
+
+            Ray ray = new Ray(new Vector3(posX, posY, posZ), Vector3.down);
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(ray, out rayHit, 100.0f))
+            {
+                Instantiate(gameObjects[randomObjectIndex], rayHit.point, rotation, parentObj.transform);
+            }
+        }
     }
 }
