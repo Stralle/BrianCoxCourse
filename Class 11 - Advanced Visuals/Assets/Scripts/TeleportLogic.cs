a8188d71e9dc409579106eb4cd51fd116d0b4a86
Stralle <strahinja.rodic@hotmail.com>
2020-07-01 19:09:49 +0200

    Added post processing

--- Class 11 - Advanced Visuals/Assets/Scripts/TeleportLogic.cs
+++ Class 11 - Advanced Visuals/Assets/Scripts/TeleportLogic.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class TeleportLogic : MonoBehaviour
 {
 
-    [SerializeField] private Transform m_transformPosition;
+    [SerializeField] private Material m_daySkybox;
+    private Camera m_mainCamera;
+    private PostProcessingBehaviour m_postProcessingBehaviour;
+    private void Start()
+    {
+        m_mainCamera = Camera.main;
+        if (m_mainCamera)
+        {
+            m_postProcessingBehaviour = m_mainCamera.GetComponent<PostProcessingBehaviour>();
+            if (m_postProcessingBehaviour)
+            {
+                m_postProcessingBehaviour.enabled = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +33,18 @@ public class TeleportLogic : MonoBehaviour
     void Teleport(GameObject obj)
     {
         PlayerLogic playerLogic = (PlayerLogic) obj.GetComponent<PlayerLogic>();
-        playerLogic.Teleport();
+        if (playerLogic)
+        {
+            playerLogic.Teleport();
+            if (m_daySkybox)
+            {
+                RenderSettings.skybox = m_daySkybox;
+            }
+
+            if (m_postProcessingBehaviour)
+            {
+                m_postProcessingBehaviour.enabled = true;
+            }
+        }
     }
 }
