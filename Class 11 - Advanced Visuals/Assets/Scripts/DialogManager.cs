96052e650412f90534b865a78e79dd08d1a7a7cf
Stralle <strahinja.rodic@hotmail.com>
2020-07-02 15:10:00 +0200

    Added dialog window, image and trigger boxes

---
+++ Class 11 - Advanced Visuals/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public static DialogManager instance = null;
+
+    [SerializeField] private TextMeshProUGUI m_characterName;
+    [SerializeField] private TextMeshProUGUI m_dialogText;
+    [SerializeField] private GameObject m_dialogWindow;
+
+    private float m_displayTime;
+    private bool m_isDisplayed = false;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (m_isDisplayed)
+        {
+            m_displayTime -= Time.deltaTime;
+
+            if (m_displayTime <= 0.0f)
+            {
+                m_dialogWindow.SetActive(false);
+                m_isDisplayed = false;
+            }
+        }
+    }
+
+    public void SetText(string characterName, string dialogText, float displayTime)
+    {
+        m_dialogWindow.SetActive(true);
+        if (m_characterName)
+        {
+            m_characterName.text = characterName;
+        }
+
+        if (m_dialogText)
+        {
+            m_dialogText.text = dialogText;
+        }
+
+        m_isDisplayed = true;
+        m_displayTime = displayTime;
+    }
+
+    //public void DisableDialogWindow()
+    //{
+    //    m_dialogWindow.SetActive(false);
+    //}
+}
---
+++ Class 11 - Advanced Visuals/Assets/Scripts/DialogTriggerBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTriggerBox : MonoBehaviour
+{
+    [SerializeField] private string m_characterName;
+    [SerializeField] private string m_dialogText;
+    [SerializeField] private float m_displayTime = 3.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && DialogManager.instance)
+        {
+            DialogManager.instance.SetText(m_characterName, m_dialogText, m_displayTime);
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Player" && DialogManager.instance)
+    //    {
+    //        DialogManager.instance.DisableDialogWindow();
+    //    }
+    //}
+}
